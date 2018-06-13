de5e05b7e5e80b1a290c68a7ad90673cc2cc603e
root <ashadnasim123@gmail.com>
2018-06-13 15:56:08 +0530

    Initial commit

---
+++ app/src/main/java/appm/ashad/quiz/swipe/MainActivity.java
@@ -0,0 +1,18 @@
+package appm.ashad.quiz.swipe;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private slideadapter myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        myadapter = new slideadapter(this);
+        viewPager.setAdapter(myadapter);
+    }
+}
